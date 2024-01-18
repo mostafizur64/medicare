833c9d4d7cae52fa1c53d4afe9b6fff3b8148557
tarafdar16 <imrantarafdar16@gmail.com>
2024-01-18 14:35:00 +0600

    contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>contact</div>;
+  return <section>
+    <div className="px-4 mx-auto max-w-screen-md">
+      <h2 className="heading text-center">Contact Us</h2>
+      <p className="mb-8 lg:mb-16 font-light text-center text__para">Got a technical issue? Want to send feedback about a beta feature? Let us know.</p>
+      <form action="#" className="space-y-8">
+        <div>
+          <label htmlFor="email" className="form__label">
+            Your Email
+          </label>
+          <input type="email" id="email"placeholder="example@gmail.com" className="form__input mt-1" />
+        </div>
+        <div>
+          <label htmlFor="subject" className="form__label">
+            Subject
+          </label>
+          <input type="text" id="subject"placeholder="Let us know how can help you" className="form__input mt-1" />
+        </div>
+        <div>
+          <label htmlFor="message" className="form__label">
+            Your Message
+          </label>
+          <textarea type="text" rows={6} id="message"placeholder="leave a comment" className="form__input mt-1" />
+        </div>
+        <button className="btn rounded sm:w-fit">Submit</button>
+      </form>
+    </div>
+  </section>;
 };
 
 export default Contact;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>login</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  return (
+    <section>
+      <div className="w-full max-w-[570px] mx-auto rounded-lg shadow-md md:p-10 p-4">
+        <h3 className="text-headingColor text-[22px] leading-9 font-bold mb-10">
+          Hello! <span className="text-primaryColor">Welcome</span> Back 🍟
+        </h3>
+        <form action="" className="py-4 md:py-0">
+          <div className="mb-5">
+            <input
+              type="email"
+              placeholder="Enter Your Email"
+              name="email"
+              value={FormData.email}
+              onChange={handleInputChange}
+              className="w-full py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16x] leading-7 text-headingColor placeholder:text-textColor  cursor-pointer"
+            />
+          </div>
+          <div className="mb-5">
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={FormData.password}
+              onChange={handleInputChange}
+              className="w-full  py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16x] leading-7 text-headingColor placeholder:text-textColor  cursor-pointer"
+            />
+          </div>
+          <div className="mt-7">
+            <button className="w-full bg-primaryColor text-white text-[18px] leading-[30px] rounded-lg  py-3">
+              Login
+            </button>
+          </div>
+          <p className="mt-5 text-textColor text-center">
+            Don't have and account?
+            <Link
+              to="/register"
+              className="text-primaryColor font-medium ml-1"
+            >Register</Link>
+          </p>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Login;
